30e99b5c557e031352bb7efad089bee674bf88b6
Nathan Edwards <nathan@onedarnleyroad.com>
2016-03-10 10:15:13 +0000

    Removed redundant packages

--- generators/app/saveplugins.js
+++ generators/app/saveplugins.js
@@ -2,8 +2,8 @@
 
 var craftplugins = require('./craftplugins.js');
 var Download = require("download");
-var gulp = require('gulp');
-var vfs = require('vinyl-fs');
+
+
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 var chalk = require('chalk');
