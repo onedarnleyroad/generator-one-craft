7d7c6a908eb2f46faaf66677ff84ad14b16dc2b6
John D Wells <johndwells@gmail.com>
2016-03-11 11:11:16 +0000

    Similarly organise our db.php to be ready for multi-environment configuration.

--- generators/app/templates/craft/config/db.php
+++ generators/app/templates/craft/config/db.php
@@ -9,20 +9,44 @@
 
 $config = array(
 
-    // The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-    'server' => 'localhost',
+	// global & defaults (required when configuring different environments)
+	'*' => array(
+		// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+		'server' => '',
 
-    // The name of the database to select.
-    'database' => '<%= localdatabase %>',
+		// The name of the database to select.
+		'database' => '',
 
-    // The database username to connect with.
-    'user' => 'root',
+		// The database username to connect with.
+		'user' => '',
 
-    // The database password to connect with.
-    'password' => 'root',
+		// The database password to connect with.
+		'password' => '',
 
-    // The prefix to use when naming tables. This can be no more than 5 characters.
-    'tablePrefix' => 'craft',
+		// The prefix to use when naming tables. This can be no more than 5 characters.
+		'tablePrefix' => 'craft',
+	),
+
+	// production
+	'production' => array(),
+
+	// staging
+	'staging' => array(),
+
+	// local / development
+	'local' => array(
+		// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+		'server' => 'localhost',
+
+		// The name of the database to select.
+		'database' => '<%= localdatabase %>',
+
+		// The database username to connect with.
+		'user' => 'root',
+
+		// The database password to connect with.
+		'password' => 'root'
+	),
 
 );
 
@@ -30,17 +54,17 @@ $config = array(
 // If a local config file exists, merge
 if (is_array($localConfig = @include(CRAFT_CONFIG_PATH . 'local/db.php')))
 {
-    // does our default config already have a local key?
-    if(array_key_exists('local', $config))
-    {
-        // If so, merge what's in our local/db.php
-        $config['local'] = array_merge($config['local'], $localConfig);
-    }
-    else
-    {
-        // Otherwise, just set as our 'local' key
-        $config['local'] = $localConfig;
-    }
+	// does our default config already have a local key?
+	if(array_key_exists('local', $config))
+	{
+		// If so, merge what's in our local/db.php
+		$config['local'] = array_merge($config['local'], $localConfig);
+	}
+	else
+	{
+		// Otherwise, just set as our 'local' key
+		$config['local'] = $localConfig;
+	}
 }
 
 // return our $config back to craft
